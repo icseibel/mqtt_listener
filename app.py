906408c6be993520c414af8673fa632ffe3eb879
Igor Seibel <i.seibe@cinq.com.br>
2021-03-30 01:08:26 -0300

    Flask to start mqtt thread

--- app.py
+++ app.py
@@ -1,4 +1,25 @@
 from mqtt_listener import MqttListener
+import os
+import threading
 
-mqtt_listener = MqttListener()
-mqtt_listener.start_mqtt_listener()
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "<h1>Hello World</hi>"
+
+def call_script():
+    mqtt_listener = MqttListener()
+    mqtt_listener.start_mqtt_listener()    
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    th = threading.Thread(target=call_script, name="Thread-mqtt")
+    th.start()
+    app.run(host='0.0.0.0', port=port)
+
+    
