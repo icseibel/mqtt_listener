a1987ab3c707b224ca4c1d4bb49c47592a60fbdf
Igor Seibel <i.seibe@cinq.com.br>
2021-03-30 00:02:30 -0300

    Initial version of MQTT Listener

---
+++ app.py
@@ -0,0 +1,4 @@
+from mqtt_listener import MqttListener
+
+mqtt_listener = MqttListener()
+mqtt_listener.start_mqtt_listener()
---
+++ mongo_util.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient
+
+
+class MongoConnection():
+
+    def __init__(self):
+        self.db_client= MongoClient('mongodb+srv://m001-student:m001-mongodb-basics@cluster0.u0wyx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+        self.db = self.db_client['iot_sensors']
+
+    def post(self, collection_name, post_content):
+        collection = self.db[collection_name]
+        return collection.insert_one(post_content).inserted_id
