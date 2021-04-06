e3acde95ec879334c14957d8bea3c2d9e9d1e478
Igor Seibel <i.seibe@cinq.com.br>
2021-04-05 22:21:10 -0300

    Add CORS treatment

--- app.py
+++ app.py
@@ -2,20 +2,36 @@ from datetime import timedelta
 import os
 import threading
 import datetime
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 
 from mqtt_listener import MqttListener
 from mongo_util import MongoConnection
 
 
 app = Flask(__name__)
-
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/")
+@cross_origin()
 def index():
-    return "<h1>Hello World</hi>"
+    return "MQTT Listener v0.1"
 
-@app.route("/lasttemp")
+@app.route("/search", methods=['GET'])
+@cross_origin()
+def search():
+    result = ['last_temp', 'history_temp', 'last_humidity', 'history_humidity']
+    return jsonify(result)
+
+@app.route("/query", methods=['GET'])
+@cross_origin()
+def query():    
+    result = None
+    return jsonify(result)
+
+@app.route("/lasttemp", methods=['GET'])
+@cross_origin()
 def last_temp():
     client = MongoConnection()    
     result = list(client.get_one(collection_name='temp', sort_field='datetime'))
@@ -23,6 +39,7 @@ def last_temp():
     return jsonify(final)
     
 @app.route("/groupedbyhour", methods=['GET'])
+@cross_origin()
 def get_grouped_by_hour():
     query = [{
     '$match': {
@@ -73,6 +90,6 @@ def call_script():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    th = threading.Thread(target=call_script, name="Thread-mqtt")
-    th.start()
+#    th = threading.Thread(target=call_script, name="Thread-mqtt")
+#    th.start()
     app.run(host='0.0.0.0', port=port)
